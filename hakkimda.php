5e8a35e89f858c8a5e2cdf7631ba8373917a71ba
Sercan Eraslan <info@sercaneraslan.com>
2012-12-22 17:21:00 +0200

    hakkimda is edited

--- hakkimda.php
+++ hakkimda.php
@@ -13,9 +13,8 @@
                     <li><a href="mailto:info@sercaneraslan.com" title="İletişim">İletişim</a></li>
                     <li><a href="http://www.linkedin.com/pub/sercan-eraslan/1b/721/618" title="LinkedIn">LinkedIn</a></li>
                     <li><a href="https://github.com/skatersercan" title="Github">Github</a></li>
+                    <li><a href="https://bitbucket.org/skatersercan" title="Bitbucket">Bitbucket</a></li>
                     <li><a href="https://twitter.com/skatersercan" title="Twitter">Twitter</a></li>
-                    <li><a href="https://www.facebook.com/sercaneraslann" title="Facebook">Facebook</a></li>
-                    <li><a href="http://about.me/sercaneraslan" title="Aboutme">Aboutme</a></li>
                 </ul>
             </section>
             <!-- #aboutMe -->
