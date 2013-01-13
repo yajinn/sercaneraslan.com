db255b66b796ce321d80942c7d1ac01d157740dc
Sercan Eraslan <info@sercaneraslan.com>
2013-01-13 20:04:06 +0200

    haller.js projesi eklendi

--- projeler.php
+++ projeler.php
@@ -19,6 +19,11 @@
                         <p><a href="http://skatersercan.github.com/Genius/">Genius</a> / <a href="https://github.com/skatersercan/Genius">Github</a></p>
                     </li>
                     <li>
+                        <h2>Haller.js</h2>
+                        <p>İsimlerin hal ve iyelik eklerini bulan JavaScript kütüphanesi.</p>
+                        <p><a href="http://skatersercan.github.com/Haller.js/">Haller.js</a> / <a href="https://github.com/skatersercan/Haller.js">Github</a></p>
+                    </li>
+                    <li>
                         <h2>Selightbox</h2>
                         <p>Javascript ve CSS ile yazılmış hızlı çalışan bir lightbox.</p>
                         <p><a href="http://skatersercan.github.com/Selightbox/">Selightbox</a> / <a href="https://github.com/skatersercan/selightbox">Github</a></p>
